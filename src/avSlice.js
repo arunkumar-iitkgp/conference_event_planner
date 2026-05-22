db480099881559b15dad49e6252e952616afdd6a
arunkumar <arunkumar.m20.iitkgp@gmail.com>
2026-05-22 22:11:44 +0530

    commit 1 : conference_event_planner

--- src/avSlice.js
+++ src/avSlice.js
@@ -3,17 +3,57 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
+    	{
+		img: "Projector.jpg",
+		name: "Projectors",
+		cost: 200,
+		quantity: 0,
+	},
+    {
+		img: "Speakers.jpg",
+		name: "Speaker",
+		cost: 35,
+		quantity: 0,
+    },
+    {
+		img: "Microphone.jpg",
+		name: "Microphones",
+		cost: 45,
+		quantity: 0,
+    },
+    {
+		img: "Whiteboard.jpg",
+		name: "Whiteboards",
+		cost: 80,
+		quantity: 0,
+    },
+
+    {
+		img: "Signs.jpg",
+		name: "Signage",
+		cost: 80,
+		quantity: 0,
+    },
+
     
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
-    },
+      const item = state[action.payload];
+      if (item) {
+        item.quantity++;
+      }
+},
+
     decrementAvQuantity: (state, action) => {
-     
-    },
+	    const item = state[action.payload];
+	    if (item && item.quantity > 0) {
+		    item.quantity--;
+	  }
+},
+
   },
 });
 
